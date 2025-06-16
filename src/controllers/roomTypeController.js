d19074273846b33143a99609ec9cbcb17661dc76
Hoang-hai-yen <23521847@gm.uit.edu.vn>
2025-06-16 15:46:02 +0700

    Fix to fit with db

--- src/controllers/roomTypeController.js
+++ src/controllers/roomTypeController.js
@@ -1,36 +1,44 @@
 const db = require('../db');
 
 // GET all room types
-exports.getRoomTypes = (req, res) => {
-  db.query('SELECT * FROM RoomType', (err, results) => {
+exports.getroomtypes = (req, res) => {
+  db.query('SELECT * FROM roomtypes', (err, results) => {
     if (err) return res.status(500).json(err);
     res.json(results);
   });
 };
 
 // POST create room type
-exports.createRoomType = (req, res) => {
-  const { roomTypeID, roomTypeName, roomSize, bed, note, maxGuests, roomPrice, surchargeRate } = req.body;
-  db.query('INSERT INTO RoomType (roomTypeID, roomTypeName, roomSize, bed, note, maxGuests, roomPrice, surchargeRate) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [roomTypeID, roomTypeName, roomSize, bed, note, maxGuests, roomPrice, surchargeRate], (err, result) => {
-    if (err) return res.status(500).json(err);
-    res.status(201).json({ roomTypeID, roomTypeName, roomSize, bed, note, maxGuests, roomPrice, surchargeRate });
-  });
+exports.createroomtypes = (req, res) => {
+  const { room_type_id, room_type_name, room_size, bed, note, max_guests, price_room, surcharge_rate } = req.body;
+  db.query(
+    'INSERT INTO roomtypes (room_type_id, room_type_name, room_size, bed, note, max_guests, price_room, surcharge_rate) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+    [room_type_id, room_type_name, room_size, bed, note, max_guests, price_room, surcharge_rate],
+    (err, result) => {
+      if (err) return res.status(500).json(err);
+      res.status(201).json({ room_type_id, room_type_name, room_size, bed, note, max_guests, price_room, surcharge_rate });
+    }
+  );
 };
 
 // PUT update room type
-exports.updateRoomType = (req, res) => {
-  const { roomTypeID } = req.params;
-  const { roomTypeName, roomSize, bed, note, maxGuests, roomPrice, surchargeRate } = req.body;
-  db.query('UPDATE RoomType SET roomTypeName=?, roomSize=?, bed=?, note=?, maxGuests=?, roomPrice=?, surchargeRate=? WHERE roomTypeID=?', [roomTypeName, roomSize, bed, note, maxGuests, roomPrice, surchargeRate, roomTypeID], (err) => {
-    if (err) return res.status(500).json(err);
-    res.json({ roomTypeID, roomTypeName, roomSize, bed, note, maxGuests, roomPrice, surchargeRate });
-  });
+exports.updateroomtypes = (req, res) => {
+  const { room_type_id } = req.params;
+  const { room_type_name, room_size, bed, note, max_guests, price_room, surcharge_rate } = req.body;
+  db.query(
+    'UPDATE roomtypes SET room_type_name=?, room_size=?, bed=?, note=?, max_guests=?, price_room=?, surcharge_rate=? WHERE room_type_id=?',
+    [room_type_name, room_size, bed, note, max_guests, price_room, surcharge_rate, room_type_id],
+    (err) => {
+      if (err) return res.status(500).json(err);
+      res.json({ room_type_id, room_type_name, room_size, bed, note, max_guests, price_room, surcharge_rate });
+    }
+  );
 };
 
 // DELETE room type
-exports.deleteRoomType = (req, res) => {
-  const { roomTypeID } = req.params;
-  db.query('DELETE FROM RoomType WHERE roomTypeID=?', [roomTypeID], (err) => {
+exports.deleteroomtypes = (req, res) => {
+  const { room_type_id } = req.params;
+  db.query('DELETE FROM roomtypes WHERE room_type_id=?', [room_type_id], (err) => {
     if (err) return res.status(500).json(err);
     res.json({ message: 'Room type deleted' });
   });
--- src/routes/roomTypeRoute.js
+++ src/routes/roomTypeRoute.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const roomTypeController = require('../controllers/roomTypeController');
 
-router.get('/', roomTypeController.getRoomTypes);
-router.post('/', roomTypeController.createRoomType);
-router.put('/:roomTypeID', roomTypeController.updateRoomType);
-router.delete('/:roomTypeID', roomTypeController.deleteRoomType);
+router.get('/', roomTypeController.getroomtypes);
+router.post('/', roomTypeController.createroomtypes);
+router.put('/:roomTypeID', roomTypeController.updateroomtypes);
+router.delete('/:roomTypeID', roomTypeController.deleteroomtypes);
 
 module.exports = router;
